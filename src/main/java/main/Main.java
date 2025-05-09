d2bfa869492e2a2669ddf6ac4b162ea3119ac1d6
schakaloff <filipshakalau@gmail.com>
2025-05-08 21:14:18 -0700

    Origin

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Workshop");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module main {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires java.desktop;
+
+    opens main to javafx.fxml;
+    exports main;
+}
