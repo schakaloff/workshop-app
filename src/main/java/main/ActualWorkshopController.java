73b5389e0ee0cd139ddec0001e4ece3573b5a75e
schakaloff <filipshakalau@gmail.com>
2025-05-12 00:06:14 -0700

    added TableView with database connection

--- src/main/java/main/ActualWorkshopController.java
+++ src/main/java/main/ActualWorkshopController.java
@@ -1,14 +1,17 @@
 package main;
 
-
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDrawer;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
@@ -16,15 +19,24 @@ import javafx.stage.Stage;
 
 import javafx.scene.input.MouseEvent;
 import java.io.IOException;
+import java.sql.*;
 
 public class ActualWorkshopController{
     @FXML
     private Label welcomeTech;
     @FXML
     private Circle techAvatar;
-
     @FXML
     private JFXButton signOutBtn;
+
+    @FXML private TableView<WorkOrder> ordersTable; //whole TableView
+    @FXML private TableColumn<WorkOrder,String> colWorkorderNumber; //first column
+    @FXML private TableColumn<WorkOrder, String> colStatus; //second
+    @FXML private TableColumn<WorkOrder, String> colDescription; //third
+
+    private final ObservableList<WorkOrder> data = FXCollections.observableArrayList();
+
+
 
 
 
@@ -32,6 +44,32 @@ public class ActualWorkshopController{
     public void initialize(){
         welcomeTech.setText(LoginController.tech); //welcome tech's name
         avatar(techAvatar); //set avatar's pic
+
+        colWorkorderNumber.setCellValueFactory(c -> c.getValue().workorderNumberProperty());
+        colStatus.setCellValueFactory(c -> c.getValue().statusProperty());
+        colDescription.setCellValueFactory(c -> c.getValue().descriptionProperty());
+
+        loadOrders();
+        ordersTable.setItems(data);
+    }
+
+    public void loadOrders(){
+        String sql = "SELECT workorder, status, item_desc FROM work_order";
+        try{
+            Connection connection = DriverManager.getConnection(DbConfig.url, DbConfig.user, DbConfig.password);
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            data.clear();
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()){
+                data.add(new WorkOrder(
+                        rs.getString("workorder"),
+                        rs.getString("status"),
+                        rs.getString("item_desc")
+                ));
+            }
+        }catch (SQLException e){
+            System.out.println("sql error");
+        }
     }
 
     public void signOut(MouseEvent e) throws IOException { //sign out button
---
+++ src/main/java/main/WorkOrder.java
@@ -0,0 +1,63 @@
+package main;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class WorkOrder {
+    /*
+    We use StringProperty because they will let watch and update UI automatically (but its an abbstract class)
+    there for we use SimpleStringProperty (like a build in array);
+     */
+    private final StringProperty workorderNumber;
+    private final StringProperty status;
+    private final StringProperty description;
+
+    public WorkOrder(String workorderNumber, String status, String description){
+        this.workorderNumber = new SimpleStringProperty(workorderNumber);
+        this.status = new SimpleStringProperty(status);
+        this.description = new SimpleStringProperty(description);
+        }
+        /*
+        for java fx there are 3 steps -> 1)get accessor, 2)getters, and 3)setters
+        */
+    //1)
+    public StringProperty workorderNumberProperty(){ //accessor
+        return workorderNumber;
+    }
+    //2)
+    public String getWorkorderNumber(){ //get
+        return workorderNumber.get();
+    }
+    //3)
+    public void setWorkorderNumber(String value){ //set
+        workorderNumber.set(value);
+    }
+
+    public StringProperty statusProperty(){
+        return status;
+    }
+
+    public String getStatus(){
+        return status.get();
+    }
+
+    public void setStatus(String value){
+        status.set(value);
+    }
+
+    public StringProperty descriptionProperty(){
+        return description;
+    }
+
+    public String getDescription(){
+        return description.get();
+    }
+
+    public void setDescription(String value){
+        description.set(value);
+    }
+
+
+
+
+}
