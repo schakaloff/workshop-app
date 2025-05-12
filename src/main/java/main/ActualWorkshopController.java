c102d532b2e815b1ee2a13f79bd87275315cf4e8
schakaloff <filipshakalau@gmail.com>
2025-05-11 21:47:48 -0700

    big changes with drawer

--- src/main/java/main/ActualWorkshopController.java
+++ src/main/java/main/ActualWorkshopController.java
@@ -1,25 +1,55 @@
 package main;
 
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDrawer;
-import com.jfoenix.controls.JFXHamburger;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 
 import javafx.scene.input.MouseEvent;
+import java.io.IOException;
 
 public class ActualWorkshopController{
     @FXML
-    private JFXDrawer drawer;
+    private Label welcomeTech;
     @FXML
-    private JFXHamburger hamburger;
-    @FXML
+    private Circle techAvatar;
 
-    public void closeMenu(MouseEvent e){
-        if(drawer.isOpened()){
-            drawer.close();
-        }else{
-            drawer.open();
-        }
+    @FXML
+    private JFXButton signOutBtn;
+
+
+
+
+    public void initialize(){
+        welcomeTech.setText(LoginController.tech); //welcome tech's name
+        avatar(techAvatar); //set avatar's pic
     }
+
+    public void signOut(MouseEvent e) throws IOException { //sign out button
+        LoginController.tech = null;
+        Parent root = FXMLLoader.load(Main.class.getResource("login.fxml"));
+        Stage stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public void avatar(Circle techAvatar){
+        Image im = new Image("/avatar.png"); //make
+        techAvatar.setCenterX(50);
+        techAvatar.setCenterY(50);
+        techAvatar.setFill(new ImagePattern(im));
+    }
+
 
 }
