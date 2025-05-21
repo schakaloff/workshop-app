39ce7027ddbe16c9c1f3474dfa2f5eeba6ddd345
schakaloff <filipshakalau@gmail.com>
2025-05-21 00:03:07 -0700

    fixed issue with crashes during creating new order

---
+++ src/main/java/main/newOrderController.java
@@ -0,0 +1,21 @@
+package main;
+
+import io.github.palexdev.materialfx.dialogs.MFXGenericDialog;
+import javafx.animation.FadeTransition;
+import javafx.animation.ParallelTransition;
+import javafx.animation.ScaleTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+import java.io.IOException;
+
+
+
+public class newOrderController {
+
+
+}
