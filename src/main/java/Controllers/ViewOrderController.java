09ab7928da2eb1be65444196fc170d7cd8dad1cd
schakaloff <filipshakalau@gmail.com>
2025-06-28 00:23:47 -0700

    started invoice system and some quality of life design in view order

--- src/main/java/Controllers/ViewOrderController.java
+++ src/main/java/Controllers/ViewOrderController.java
@@ -8,14 +8,19 @@ import io.github.palexdev.materialfx.controls.MFXTextField;
 import io.github.palexdev.materialfx.dialogs.MFXGenericDialog;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.stage.Window;
 import print.Print;
 
 
+import java.io.IOException;
 import java.sql.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -44,16 +49,33 @@ public class ViewOrderController {
     @FXML private TabPane tabPane;
     @FXML private TextArea serviceNotesTXT;
 
+
+    //main
+    @FXML private MFXTextField mainNumberTFX;
+
+    //labour
+    @FXML private MFXTextField customerTFX;
+    @FXML private MFXTextField statusTFX;
+    @FXML private MFXTextField numberTFX;
+
+    //parts
+    @FXML private MFXTextField partsCustomerTFX;
+    @FXML private MFXTextField partsStatusTFX;
+    @FXML private MFXTextField partsNumberTFX;
+
     private WorkOrder currentWorkOrder;
     private Customer currentCustomer;
 
     public void setMainController(ActualWorkshopController controller) {this.mainController = controller;}
     public void setDialogInstance(MFXGenericDialog dialogInstance) {this.dialogInstance = dialogInstance;}
-
     public void initialize(){tabPane.setFocusTraversable(false);}
 
     public void initData(WorkOrder wo, Customer co){
-        //info tab
+        String firstName = co.getFirstName();
+        String lastName = co.getLastName();
+        String fullName = lastName + "," + firstName;
+
+        //main tab
         status.setText(wo.getStatus());
 
         type.setText(wo.getType());
@@ -72,19 +94,75 @@ public class ViewOrderController {
         townTFX.setText(co.getTown());
         zipTFX.setText(co.getPostalCode());
 
+        mainNumberTFX.setText((String.valueOf(wo.getWorkorderNumber())));
+
         this.currentWorkOrder = wo;
         this.currentCustomer = co;
 
-        //work tab
+        //labour tab
+        customerTFX.setText(fullName);
+        statusTFX.setText(wo.getStatus());
+        numberTFX.setText((String.valueOf(wo.getWorkorderNumber())));
 
         loadServiceNotes();
-
         serviceNotesTXT.addEventFilter(MouseEvent.MOUSE_CLICKED, e->{ //attach mouse event to service notes function
             if(e.getButton() == MouseButton.PRIMARY){
                 insertNotes(serviceNotesTXT);
                 e.consume();
             }
         });
+
+        //parts tab
+        partsCustomerTFX.setText(fullName);
+        partsStatusTFX.setText(wo.getStatus());
+        partsNumberTFX.setText(String.valueOf(wo.getWorkorderNumber()));
+
+    }
+
+    public void insertNotes(TextArea area){  //service notes function
+        String stamp = "[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")) + "]: ";
+        String text = area.getText();
+        int caret = area.getCaretPosition();
+
+        if (caret > 0 && text.charAt(caret - 1) != '\n') {
+            stamp = "\n" + stamp;
+        }
+        area.insertText(caret, stamp);
+        area.positionCaret(caret + stamp.length());
+    }
+
+    @FXML
+    public void Pay() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/main/pay.fxml"));
+        MFXGenericDialog dialog = loader.load();
+        Stage dialogStage = new Stage();
+        /*
+        we are telling javafx that new stage should be modal
+        It will prevent user from interacting with other windows.
+        Modality.APPLICATION blocks mouse and keyboard input to all other windows in this app.
+         */
+        dialogStage.initModality(Modality.APPLICATION_MODAL);
+        dialogStage.setTitle("Payment");
+        Scene scene = new Scene(dialog);
+        dialogStage.setScene(scene);
+        dialogStage.showAndWait();
+    }
+
+    public void loadServiceNotes(){
+        String sql = "SELECT service_notes FROM work_order WHERE workorder = ?";
+        try{
+            Connection conn = DriverManager.getConnection(DbConfig.url, DbConfig.user, DbConfig.password);
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, currentWorkOrder.getWorkorderNumber());
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                serviceNotesTXT.setText(rs.getString("service_notes"));
+            }else{
+                serviceNotesTXT.clear();
+            }
+        }catch (SQLException e){
+            System.out.println("issue during loading service notes");
+        }
     }
 
     @FXML
@@ -105,36 +183,6 @@ public class ViewOrderController {
             System.out.println("error during closing the order");
         }
         status.setText(currentWorkOrder.getStatus());
-    }
-
-
-    public void insertNotes(TextArea area){  //service notes function
-        String stamp = "[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")) + "]: ";
-        String text = area.getText();
-        int caret = area.getCaretPosition();
-
-        if (caret > 0 && text.charAt(caret - 1) != '\n') {
-            stamp = "\n" + stamp;
-        }
-        area.insertText(caret, stamp);
-        area.positionCaret(caret + stamp.length());
-    }
-
-    public void loadServiceNotes(){
-        String sql = "SELECT service_notes FROM work_order WHERE workorder = ?";
-        try{
-            Connection conn = DriverManager.getConnection(DbConfig.url, DbConfig.user, DbConfig.password);
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setInt(1, currentWorkOrder.getWorkorderNumber());
-            ResultSet rs = ps.executeQuery();
-            if(rs.next()){
-                serviceNotesTXT.setText(rs.getString("service_notes"));
-            }else{
-                serviceNotesTXT.clear();
-            }
-        }catch (SQLException e){
-            System.out.println("issue during loading service notes");
-        }
     }
 
     @FXML
