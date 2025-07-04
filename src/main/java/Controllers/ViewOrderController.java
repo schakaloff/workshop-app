50fd9c83cda91089ce57386b676ac704b854e4b6
schakaloff <filipshakalau@gmail.com>
2025-07-04 00:38:59 -0700

    insert the date, tech id, description and the pay amount at the labour tab

--- src/main/java/Controllers/ViewOrderController.java
+++ src/main/java/Controllers/ViewOrderController.java
@@ -3,16 +3,18 @@ package Controllers;
 import DB.DbConfig;
 import Skeletons.Customer;
 import Skeletons.WorkOrder;
-import io.github.palexdev.materialfx.controls.MFXComboBox;
-import io.github.palexdev.materialfx.controls.MFXTableView;
-import io.github.palexdev.materialfx.controls.MFXTextField;
+import Skeletons.WorkTable;
+import io.github.palexdev.materialfx.controls.*;
+import io.github.palexdev.materialfx.controls.cell.MFXTableRowCell;
 import io.github.palexdev.materialfx.dialogs.MFXGenericDialog;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Modality;
@@ -23,6 +25,7 @@ import print.Print;
 
 import java.io.IOException;
 import java.sql.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -51,7 +54,6 @@ public class ViewOrderController {
     @FXML private TabPane tabPane;
     @FXML private TextArea serviceNotesTXT;
 
-
     //main
     @FXML private MFXTextField mainNumberTFX;
 
@@ -59,7 +61,9 @@ public class ViewOrderController {
     @FXML private MFXTextField customerTFX;
     @FXML private MFXTextField statusTFX;
     @FXML private MFXTextField numberTFX;
-    @FXML private MFXTableView table;
+
+    @FXML private MFXTableView<WorkTable> table;
+    private final ObservableList<WorkTable> data = FXCollections.observableArrayList();
 
     //parts
     @FXML private MFXTextField partsCustomerTFX;
@@ -71,9 +75,68 @@ public class ViewOrderController {
 
     public void setMainController(ActualWorkshopController controller) {this.mainController = controller;}
     public void setDialogInstance(MFXGenericDialog dialogInstance) {this.dialogInstance = dialogInstance;}
+
     public void initialize(){
         tabPane.setFocusTraversable(false);
         table.setFooterVisible(false);
+        loadRepairTable();
+        table.setItems(data);
+
+    }
+
+    public void loadRepairTable() {
+        table.getTableColumns().clear();
+        data.clear();
+
+        MFXTableColumn<WorkTable> dateCol = new MFXTableColumn<>("Date");
+        dateCol.setPrefWidth(150);
+        dateCol.setRowCellFactory(item -> {
+            MFXTableRowCell<WorkTable, LocalDate> cell = new MFXTableRowCell<>(WorkTable::getDate);
+            DatePicker picker = new DatePicker();
+            cell.setGraphic(picker);
+            cell.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+            return cell;
+        });
+
+        MFXTableColumn<WorkTable> techCol = new MFXTableColumn<>("Tech");
+        techCol.setRowCellFactory(item->{
+            MFXTableRowCell<WorkTable,String> cell = new MFXTableRowCell<>(WorkTable::getTech);
+            ComboBox box = new ComboBox();
+            cell.setGraphic(box);
+            cell.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+            return cell;
+        });
+
+        MFXTableColumn<WorkTable> descCol = new MFXTableColumn<>("Description" );
+        descCol.setPrefWidth(300);
+        descCol.setRowCellFactory(item->{
+            MFXTableRowCell<WorkTable, String> cell = new MFXTableRowCell<>(WorkTable::getDescription);
+            TextArea area = new TextArea();
+            area.setWrapText(true);
+            cell.setGraphic(area);
+            cell.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+            return cell;
+        });
+
+        MFXTableColumn<WorkTable> priceCol = new MFXTableColumn<>("Price" );;
+        priceCol.setRowCellFactory(item->{
+            MFXTableRowCell<WorkTable,Double> cell = new MFXTableRowCell<>(WorkTable::getPrice);
+            TextField field = new TextField();
+            field.setAlignment(Pos.CENTER_RIGHT);
+            cell.setGraphic(field);
+            cell.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+            return cell;
+        });
+
+        data.add(new WorkTable(LocalDate.now(), "", "", 0.0));
+        table.getTableColumns().addAll(dateCol, techCol, descCol, priceCol);
+
+        table.setItems(data);
+    }
+
+    @FXML
+    public void onAddStep() {
+        data.add(new WorkTable(LocalDate.now(), "", "", 0.0));
     }
 
     public void initData(WorkOrder wo, Customer co){
@@ -214,7 +277,7 @@ public class ViewOrderController {
             ps.setString(5, newVendorId);
             ps.setString(6, newWarrantyNumber);
             ps.setString(7, newServiceNotes);
-            ps.setInt(8, woNumber);
+             ps.setInt(8, woNumber);
             int updated = ps.executeUpdate();
             ps.close();
             conn.close();
