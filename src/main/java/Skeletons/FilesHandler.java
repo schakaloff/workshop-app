7adc7a2efc14c8129436e9178cd48eecefee7889
schakaloff <filipshakalau@gmail.com>
2025-08-17 17:16:57 -0700

    adjustments

--- src/main/java/Skeletons/FilesHandler.java
+++ src/main/java/Skeletons/FilesHandler.java
@@ -6,11 +6,11 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
-public class Files {
+public class FilesHandler {
     private final IntegerProperty id ;
     private final StringProperty name;
 
-    public Files(int id, String name){
+    public FilesHandler(int id, String name){
         this.id = new SimpleIntegerProperty(id);
         this.name = new SimpleStringProperty(name);
     }
