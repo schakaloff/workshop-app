cb46eb6e7144430249ed42b0e702a075b337f66d
schakaloff <filipshakalau@gmail.com>
2026-01-12 01:25:12 -0800

    some changes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,9 @@
 module main {
-    requires javafx.fxml;
     requires java.sql;
     requires com.jfoenix;
     requires javafx.controls;
     requires MaterialFX;
-    requires org.apache.poi.ooxml;
-
+    //requires org.apache.poi.ooxml;
     opens main to javafx.fxml;
     exports main;
     exports Skeletons;
