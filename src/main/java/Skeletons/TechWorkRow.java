c9cd3b236a08adfd0ea67274f943d18c7ad0efab
schakaloff <filipshakalau@gmail.com>
2026-03-09 17:18:28 -0700

    tech stats + simple code refactoring

--- src/main/java/Skeletons/TechWorkRow.java
+++ src/main/java/Skeletons/TechWorkRow.java
@@ -2,19 +2,25 @@ package Skeletons;
 
 public class TechWorkRow {
     private final int workOrderNumber;
+    private final String type;
     private final String status;
     private final double labourAmount;
-    private final String device;
+    private final String finishedDate;
 
-    public TechWorkRow(int workOrderNumber, String status, double labourAmount, String device) {
+    public TechWorkRow(int workOrderNumber, String type, String status, double labourAmount, String finishedDate) {
         this.workOrderNumber = workOrderNumber;
+        this.type = type;
         this.status = status;
         this.labourAmount = labourAmount;
-        this.device = device;
+        this.finishedDate = finishedDate;
     }
 
     public int getWorkOrderNumber() {
         return workOrderNumber;
+    }
+
+    public String getType() {
+        return type;
     }
 
     public String getStatus() {
@@ -25,7 +31,7 @@ public class TechWorkRow {
         return labourAmount;
     }
 
-    public String getDevice() {
-        return device;
+    public String getFinishedDate() {
+        return finishedDate;
     }
 }
