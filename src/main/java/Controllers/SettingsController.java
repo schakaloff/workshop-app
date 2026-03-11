cd0e00ef6cb2f9a3c24a1a2d2960f61732145f25
schakaloff <filipshakalau@gmail.com>
2026-03-11 10:38:05 -0700

    some code

--- src/main/java/Controllers/SettingsController.java
+++ src/main/java/Controllers/SettingsController.java
@@ -3,7 +3,12 @@ package Controllers;
 import Skeletons.Technicians;
 import io.github.palexdev.materialfx.controls.MFXTableView;
 import io.github.palexdev.materialfx.dialogs.MFXGenericDialog;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import utils.TableMethods;
 
 public class SettingsController {
     @FXML private ActualWorkshopController mainController;
@@ -12,21 +17,31 @@ public class SettingsController {
     @FXML private MFXTableView<Technicians> techsTable;
 
 
+
+    private final ObservableList<Technicians> techData = FXCollections.observableArrayList();
+
+
     public void setMainController(ActualWorkshopController controller) {this.mainController = controller;}
     public void setDialogInstance(MFXGenericDialog dialogInstance) {this.dialogInstance = dialogInstance;}
 
 
     public void initialize(){
-
+        loadTechs();
     }
 
+    private void loadTechs() {
+        techData.setAll(TableMethods.loadTechniciansWithRoles());
+        TableMethods.loadTechniciansRolesTable(techsTable, techData);
+    }
 
     public void addNewTech(){
 
     }
 
-    public void saveTechs(){
-
+    @FXML
+    public void saveTechs() {
+        TableMethods.saveAllTechnicianRoles(techData);
+        new Alert(Alert.AlertType.INFORMATION, "Technician roles updated successfully.", ButtonType.OK).showAndWait();
     }
 
     @FXML
